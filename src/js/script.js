2715dd2ab5b3d381eb76e148450621d54f152273
Maslovkyi Vadym <improvemylife25@gmail.com>
2020-04-18 15:30:52 +0300

    finished carousel slider

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+
+                }
+            }
+           
+          ]
+    });
+  });
